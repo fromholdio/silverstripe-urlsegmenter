3404decd0e0f87969270606da7e57f453a94c4c5
Luke Fromhold <luke@fromhold.io>
2021-12-17 16:22:24 +1100

    Initial build for v3 - configurable source and storage field names, with option to get source value from method instead

--- src/Extensions/URLSegmenter.php
+++ src/Extensions/URLSegmenter.php
@@ -3,15 +3,74 @@
 namespace Fromholdio\URLSegmenter\Extensions;
 
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\FormField;
 use SilverStripe\Forms\ReadonlyField;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\ORM\DataList;
+use SilverStripe\ORM\FieldType\DBField;
 use SilverStripe\View\Parsers\URLSegmentFilter;
+use SilverStripe\Forms\TextField;
 
 class URLSegmenter extends DataExtension
 {
-    private static $urlsegmenter_force_title = true;
-    private static $urlsegmenter_enable_field = false;
+    /**
+     * Should URLSegment be forced to match source value
+     * @var bool
+     */
+    private static $urlsegmenter_forced = true;
+
+    /**
+     * Sets URLSegmenterFilter to replace or transliterate non-ASCII filters.
+     * @var bool
+     * @see URLSegmentFilter::$default_allow_multibyte
+     */
+    private static $urlsegmenter_allow_multibyte = true;
+
+    /**
+     * DB field in which to store the resulting URLSegment value.
+     * --
+     * Defaults to 'URLSegment', and this extension adds that db field.
+     * If you change this, you need to ensure you also add the
+     * associated db field.
+     * @var string
+     */
+    private static $urlsegmenter_field_name = 'URLSegment';
+
+    /**
+     * $db field to get value for initial URLSegment generation
+     * Ignored if $urlsegmenter_source_method_name is set.
+     * @var string
+     */
+    private static $urlsegmenter_source_field_name = 'Title';
+
+    /**
+     * Method name to get source value for URLSegment generation.
+     * - Overrides $urlsegmenter_source_field_name if set.
+     * - Expected value is method name without "->" or "()".
+     * - Object::hasMethod($methodName) should return true.
+     * @var null
+     */
+    private static $urlsegmenter_source_method_name = null;
+
+    /**
+     * Display URLSegment field in CMSFields.
+     * --
+     * If $urlsegmenter_forced is true, the field will only
+     * be displayed after the record is in the database, and will
+     * be a ReadOnlyField.
+     * @var bool|string
+     */
+    private static $urlsegmenter_cmsfield_enabled = false;
+
+    /**
+     * Provide name of CMS field to be inserted before or after.
+     * If null, will be inserted after source_field if exists
+     * (else not at all).
+     * @var null|string
+     */
+    private static $urlsegmenter_cmsfield_insertbefore = null;
+    private static $urlsegmenter_cmsfield_insertafter = null;
+
 
     private static $db = [
         'URLSegment' => 'Varchar(255)'
@@ -21,43 +80,206 @@ class URLSegmenter extends DataExtension
         'URLSegment' => 'URL Segment'
     ];
 
-    public function updateCMSFields(FieldList $fields)
+
+    public function isURLSegmenterForced(): bool
     {
-        $fields->removeByName('URLSegment');
-
-        $enableField = $this->getOwner()->config()->get('urlsegmenter_enable_field');
-        if (!$enableField) {
-            return;
-        }
-
-        if ($this->getOwner()->URLSegment) {
-            $urlSegmentField = ReadonlyField::create(
-                'URLSegment',
-                'URL Segment',
-                $this->getOwner()->URLSegment
-            );
-            $urlSegmentField->setDescription('Based on Title. Special characters are automatically converted or removed.');
-            $fields->insertAfter('Title', $urlSegmentField);
-        }
+        return (bool) $this->getOwner()->config()->get('urlsegmenter_forced');
     }
 
-    public function onBeforeWrite()
+    public function isURLSegmenterAllowMultibyte(): bool
     {
-        if ($this->getOwner()->URLSegment) {
-            $forceTitle = $this->getOwner()->config()->get('urlsegmenter_force_title');
-            if ($forceTitle && $this->getOwner()->isChanged('Title')) {
-                $this->getOwner()->generateURLSegment();
+        return (bool) $this->getOwner()->config()->get('urlsegmenter_allow_multibyte');
+    }
+
+    public function getURLSegmenterFieldName(): string
+    {
+        $name = $this->getOwner()->config()->get('urlsegmenter_field_name');
+        if (empty($name)) {
+            throw new \UnexpectedValueException(
+                'The $urlsegmenter_field_name on ' . get_class($this->getOwner()) . ' is empty.'
+            );
+        }
+
+        $dbObject = $this->getOwner()->dbObject($name);
+        if (!$dbObject) {
+            throw new \UnexpectedValueException(
+                'The $urlsegmenter_field_name on ' . get_class($this->getOwner())
+                . ' does not match a valid dbObject name.'
+            );
+        }
+        return $name;
+    }
+
+    public function getURLSegmenterSourceMethodName(): ?string
+    {
+        $method = $this->getOwner()->config()->get('urlsegmenter_source_method_name');
+        $method = empty($method) ? null : $method;
+        if (!$this->getOwner()->hasMethod($method)) {
+            throw new \UnexpectedValueException(
+                'The $urlsegmenter_source_method_name on ' . get_class($this->getOwner())
+                . ' does not match a valid method name.'
+            );
+        }
+        return $method;
+    }
+
+    public function getURLSegmenterSourceFieldName(): ?string
+    {
+        $name = null;
+        $method = $this->getOwner()->getURLSegmenterSourceMethodName();
+        if (is_null($method))
+        {
+            $fieldName = $this->getOwner()->config()->get('urlsegmenter_source_field_name');
+            if (empty($fieldName)) {
+                throw new \UnexpectedValueException(
+                    'The $urlsegmenter_source_field_name on ' . get_class($this->getOwner())
+                    . ' is empty (and no $urlsegmenter_source_method_name is set either).'
+                );
+            }
+
+            $dbObject = $this->getOwner()->dbObject($name);
+            if (!$dbObject) {
+                throw new \UnexpectedValueException(
+                    'The $urlsegmenter_source_field_name on ' . get_class($this->getOwner())
+                    . ' does not match a valid dbObject name.'
+                );
+            }
+            $name = $fieldName;
+        }
+        return $name;
+    }
+
+    public function getURLSegmenterSourceValue(): string
+    {
+        $value = null;
+        $method = $this->getOwner()->getURLSegmenterSourceMethodName();
+        if (!is_null($method)) {
+            $value = $this->getOwner()->{$method}();
+        }
+        else {
+            $name = $this->getOwner()->getURLSegmenterSourceFieldName();
+            $value = $this->getOwner()->{$name};
+        }
+        if (empty($value)) {
+            $value = $this->getOwner()->getURLSegmenterDefaultValue();
+        }
+        return $value;
+    }
+
+    public function getURLSegmenterDefaultValue(): string
+    {
+        $value = _t(
+            'URLSEGMENTER.NEWSOURCEVALUE',
+            'New {singularName}',
+            ['singularName' => $this->getOwner()->i18n_singular_name()]
+        );
+        $this->getOwner()->invokeWithExtensions('updateURLSegmenterDefaultValue', $value);
+        return $value;
+    }
+
+
+    public function isURLSegmenterCMSFieldEnabled(): bool
+    {
+        return (bool) $this->getOwner()->config()->get('urlsegmenter_cmsfield_enabled');
+    }
+
+    public function getURLSegmenterCMSFieldInsertBefore(): ?string
+    {
+        $before = $this->getOwner()->config()->get('urlsegmenter_cmsfield_insertbefore');
+        return empty($before) ? null : $before;
+    }
+
+    public function getURLSegmenterCMSFieldInsertAfter(): ?string
+    {
+        $before = $this->getOwner()->getURLSegmenterCMSFieldInsertBefore();
+        if (!is_null($before)) {
+            return null;
+        }
+        $after = $this->getOwner()->config()->get('urlsegmenter_cmsfield_insertafter');
+        return empty($after)
+            ? $this->getOwner()->getURLSegmenterSourceFieldName()
+            : $after;
+    }
+
+
+    public function getURLSegmenterCMSField(): FormField
+    {
+        $fieldName = $this->getOwner()->getURLSegmenterFieldName();
+        $description = _t('URLSEGMENTER.SPECIALCHARSDESC', 'Special characters are automatically converted or removed.');
+        if ($this->getOwner()->isURLSegmenterForced())
+        {
+            $field = ReadonlyField::create(
+                'URLSegment',
+                $this->getOwner()->fieldLabel($fieldName),
+                $this->getOwner()->{$fieldName}
+            );
+            $name = $this->getOwner()->getURLSegmenterSourceFieldName();
+            if (!is_null($name)) {
+                $generated = _t(
+                    'URLSEGMENTER.GENERATEDFROMLABEL',
+                    'Generated from {fieldLabel}.',
+                    ['fieldLabel' => $this->getOwner()->fieldLabel($name)]
+                );
+                $description = $generated . ' ' . $description;
             }
         }
         else {
+            $field = TextField::create(
+                'URLSegment',
+                $this->getOwner()->fieldLabel($fieldName),
+                $this->getOwner()->{$fieldName}
+            );
+        }
+        $field->setDescription($description);
+        $this->getOwner()->invokeWithExtensions('updateURLSegmenterCMSField', $field);
+        return $field;
+    }
+
+    public function updateCMSFields(FieldList $fields): void
+    {
+        $fieldName = $this->getOwner()->getURLSegmenterFieldName();
+        $fields->removeByName($fieldName);
+
+        if (!$this->getOwner()->isURLSegmenterCMSFieldEnabled()) {
+            return;
+        }
+
+        if (!$this->getOwner()->isInDB() && $this->getOwner()->isURLSegmenterForced()) {
+            return;
+        }
+
+        $insertBefore = $this->getOwner()->getURLSegmenterCMSFieldInsertBefore();
+        if ($insertBefore) {
+            $urlSegmentField = $this->getOwner()->getURLSegmenterCMSField();
+            $fields->insertBefore($insertBefore, $urlSegmentField);
+        }
+        else {
+            $insertAfter = $this->getOwner()->getURLSegmenterCMSFieldInsertAfter();
+            if ($insertAfter) {
+                $urlSegmentField = $this->getOwner()->getURLSegmenterCMSField();
+                $fields->insertAfter($insertAfter, $urlSegmentField);
+            }
+        }
+    }
+
+
+    public function onBeforeWrite(): void
+    {
+        $fieldName = $this->getOwner()->getURLSegmenterFieldName();
+        if (
+            !$this->getOwner()->isInDB()
+            || empty($this->getOwner()->{$fieldName})
+            || $this->getOwner()->isURLSegmenterForced()
+        ) {
             $this->getOwner()->generateURLSegment();
         }
     }
 
-    public function generateURLSegment($increment = 0)
+
+    public function generateURLSegment($increment = 0): string
     {
         $scope = $this->getOwner()->hasMethod('getURLSegmenterScope')
-            ? $this->getOwner()->getSortableScope()
+            ? $this->getOwner()->getURLSegmenterScope()
             : get_class($this->getOwner())::get();
 
         if (is_a($scope, DataList::class) && $scope->count() > 0) {
@@ -68,25 +290,22 @@ class URLSegmenter extends DataExtension
 
         $increment = (int) $increment;
         $filter = URLSegmentFilter::create();
-        $filter->setAllowMultibyte(true);
+        $filter->setAllowMultibyte($this->getOwner()->isURLSegmenterAllowMultibyte());
 
-        $this->getOwner()->URLSegment = $filter->filter($this->getOwner()->Title);
-        if (!$this->getOwner()->URLSegment) {
-            $this->getOwner()->URLSegment = $filter->filter(
-                'New ' . $this->getOwner()->i18n_singular_name()
-            );
-        }
+        $fieldName = $this->getOwner()->getURLSegmenterFieldName();
+
+        $sourceValue = $this->getOwner()->getURLSegmenterSourceValue();
+        $this->getOwner()->{$fieldName} = $filter->filter($sourceValue);
 
         if ($increment > 0) {
-            $this->getOwner()->URLSegment .= '-' . $increment;
+            $this->getOwner()->{$fieldName} .= '-' . $increment;
         }
 
-        $duplicates = $scope->filter('URLSegment', $this->getOwner()->URLSegment);
-
+        $duplicates = $scope->filter($fieldName, $this->getOwner()->{$fieldName});
         if ($duplicates->count() > 0) {
             $this->getOwner()->generateURLSegment($increment + 1);
         }
 
-        return $this->getOwner()->URLSegment;
+        return $this->getOwner()->{$fieldName};
     }
 }
